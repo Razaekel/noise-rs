8bce1f44157f1f43c9e68aec02944219e6c074f6
tiffany <tiffany@stormbit.net>
2013-09-25 10:56:59 -0400

    Generated permutation table

--- src/noise/perlin.rs
+++ src/noise/perlin.rs
@@ -17,6 +17,8 @@
 //! (http://mrl.nyu.edu/~perlin/noise/) algorithm.
 
 use std::num::cast;
+use std::rand;
+use std::rand::Rng;
 
 pub trait Perlin<T> {
     fn perlin(&self, ctx: &PerlinContext) -> T;
@@ -79,13 +81,13 @@ impl<T: Clone + Float> Perlin<T> for [T, ..3] {
 /// A perlin noise generator
 pub struct PerlinContext {
     // permutation table
-    priv ptable: [uint, ..512],
+    priv ptable: ~[u8]//[uint, ..512],
 }
 
 impl PerlinContext {
     pub fn new() -> PerlinContext {
-        // TODO: generate random permutation table
-        PerlinContext { ptable: P }
+        let mut rng = rand::rng();
+        PerlinContext { ptable: rng.gen_vec::<u8>(512) }
     }
 
     pub fn gen1<T:Clone + Float>(&self, x: &T) -> T {
@@ -98,9 +100,9 @@ impl PerlinContext {
 
     pub fn gen3<T:Clone + Float>(&self, x: &T, y: &T, z: &T) -> T {
         // Find the unit cube that contains the point
-        let X = x.floor().to_uint() & 255;
-        let Y = y.floor().to_uint() & 255;
-        let Z = z.floor().to_uint() & 255;
+        let X = x.floor().to_uint() as u8;
+        let Y = y.floor().to_uint() as u8;
+        let Z = z.floor().to_uint() as u8;
 
         // Find the relative X, Y, Z of point in the cube
         let x_ = *x - x.floor();
@@ -121,14 +123,14 @@ impl PerlinContext {
         let BB = self.ptable[B + 1] + Z;
 
         // Add the blended results from the 8 corners of the cube
-        lerp(w, lerp(v.clone(), lerp(u.clone(), grad(self.ptable[AA    ], x_.clone(), y_.clone(), z_.clone()),
-                                                grad(self.ptable[BA    ], x_-cast(1), y_.clone(), z_.clone())),
-                                lerp(u.clone(), grad(self.ptable[AB    ], x_.clone(), y_-cast(1), z_.clone()),
-                                                grad(self.ptable[BB    ], x_-cast(1), y_-cast(1), z_.clone()))),
-                lerp(v.clone(), lerp(u.clone(), grad(self.ptable[AA + 1], x_.clone(), y_.clone(), z_-cast(1)),
-                                                grad(self.ptable[BA + 1], x_-cast(1), y_.clone(), z_-cast(1))),
-                                lerp(u.clone(), grad(self.ptable[AB + 1], x_.clone(), y_-cast(1), z_-cast(1)),
-                                                grad(self.ptable[BB + 1], x_-cast(1), y_-cast(1), z_-cast(1)))))
+        lerp(w, lerp(v.clone(), lerp(u.clone(), grad(self.ptable[AA    ] as uint, x_.clone(), y_.clone(), z_.clone()),
+                                                grad(self.ptable[BA    ] as uint, x_-cast(1), y_.clone(), z_.clone())),
+                                lerp(u.clone(), grad(self.ptable[AB    ] as uint, x_.clone(), y_-cast(1), z_.clone()),
+                                                grad(self.ptable[BB    ] as uint, x_-cast(1), y_-cast(1), z_.clone()))),
+                lerp(v.clone(), lerp(u.clone(), grad(self.ptable[AA + 1] as uint, x_.clone(), y_.clone(), z_-cast(1)),
+                                                grad(self.ptable[BA + 1] as uint, x_-cast(1), y_.clone(), z_-cast(1))),
+                                lerp(u.clone(), grad(self.ptable[AB + 1] as uint, x_.clone(), y_-cast(1), z_-cast(1)),
+                                                grad(self.ptable[BB + 1] as uint, x_-cast(1), y_-cast(1), z_-cast(1)))))
     }
 }
 
@@ -157,39 +159,3 @@ fn grad<T:Clone + Num + NumCast>(hash: uint, x: T, y: T, z: T) -> T {
     (if (h & 2) == 0 { v } else { -v })
 }
 
-/// Permutation table
-static P: [uint, ..512] = [
-    151, 160, 137,  91,  90,  15, 131,  13, 201,  95,  96,  53, 194, 233,   7, 225,
-    140,  36, 103,  30,  69, 142,   8,  99,  37, 240,  21,  10,  23, 190,   6, 148,
-    247, 120, 234,  75,   0,  26, 197,  62,  94, 252, 219, 203, 117,  35,  11,  32,
-     57, 177,  33,  88, 237, 149,  56,  87, 174,  20, 125, 136, 171, 168,  68, 175,
-     74, 165,  71, 134, 139,  48,  27, 166,  77, 146, 158, 231,  83, 111, 229, 122,
-     60, 211, 133, 230, 220, 105,  92,  41,  55,  46, 245,  40, 244, 102, 143,  54,
-     65,  25,  63, 161,   1, 216,  80,  73, 209,  76, 132, 187, 208,  89,  18, 169,
-    200, 196, 135, 130, 116, 188, 159,  86, 164, 100, 109, 198, 173, 186,   3,  64,
-     52, 217, 226, 250, 124, 123,   5, 202,  38, 147, 118, 126, 255,  82,  85, 212,
-    207, 206,  59, 227,  47,  16,  58,  17, 182, 189,  28,  42, 223, 183, 170, 213,
-    119, 248, 152,   2,  44, 154, 163,  70, 221, 153, 101, 155, 167,  43, 172,   9,
-    129,  22,  39, 253,  19,  98, 108, 110,  79, 113, 224, 232, 178, 185, 112, 104,
-    218, 246,  97, 228, 251,  34, 242, 193, 238, 210, 144,  12, 191, 179, 162, 241,
-     81,  51, 145, 235, 249,  14, 239, 107,  49, 192, 214,  31, 181, 199, 106, 157,
-    184,  84, 204, 176, 115, 121,  50,  45, 127,   4, 150, 254, 138, 236, 205,  93,
-    222, 114,  67,  29,  24,  72, 243, 141, 128, 195,  78,  66, 215,  61, 156, 180,
-
-    151, 160, 137,  91,  90,  15, 131,  13, 201,  95,  96,  53, 194, 233,   7, 225,
-    140,  36, 103,  30,  69, 142,   8,  99,  37, 240,  21,  10,  23, 190,   6, 148,
-    247, 120, 234,  75,   0,  26, 197,  62,  94, 252, 219, 203, 117,  35,  11,  32,
-     57, 177,  33,  88, 237, 149,  56,  87, 174,  20, 125, 136, 171, 168,  68, 175,
-     74, 165,  71, 134, 139,  48,  27, 166,  77, 146, 158, 231,  83, 111, 229, 122,
-     60, 211, 133, 230, 220, 105,  92,  41,  55,  46, 245,  40, 244, 102, 143,  54,
-     65,  25,  63, 161,   1, 216,  80,  73, 209,  76, 132, 187, 208,  89,  18, 169,
-    200, 196, 135, 130, 116, 188, 159,  86, 164, 100, 109, 198, 173, 186,   3,  64,
-     52, 217, 226, 250, 124, 123,   5, 202,  38, 147, 118, 126, 255,  82,  85, 212,
-    207, 206,  59, 227,  47,  16,  58,  17, 182, 189,  28,  42, 223, 183, 170, 213,
-    119, 248, 152,   2,  44, 154, 163,  70, 221, 153, 101, 155, 167,  43, 172,   9,
-    129,  22,  39, 253,  19,  98, 108, 110,  79, 113, 224, 232, 178, 185, 112, 104,
-    218, 246,  97, 228, 251,  34, 242, 193, 238, 210, 144,  12, 191, 179, 162, 241,
-     81,  51, 145, 235, 249,  14, 239, 107,  49, 192, 214,  31, 181, 199, 106, 157,
-    184,  84, 204, 176, 115, 121,  50,  45, 127,   4, 150, 254, 138, 236, 205,  93,
-    222, 114,  67,  29,  24,  72, 243, 141, 128, 195,  78,  66, 215,  61, 156, 180,
-];
