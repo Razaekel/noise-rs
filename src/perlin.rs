1ff8316c40523dfe2faf671e676d603dd6b14128
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-12-27 16:02:55 +1000

    Clean up perlin noise implementations

--- src/perlin.rs
+++ src/perlin.rs
@@ -19,23 +19,14 @@ use seed::Seed;
 use util::{lerp, scurve3, scurve5};
 use gradients::{GRADIENT2, GRADIENT3, GRADIENT4};
 
-fn get_perlin2_gradient(seed: &Seed, x_whole: int, y_whole: int, x_frac: f32, y_frac: f32) -> f32 {
-    let vector = GRADIENT2[seed.get2(x_whole, y_whole) % GRADIENT2.len()];
-    x_frac * vector[0] + y_frac * vector[1]
-}
-
-fn get_perlin3_gradient(seed: &Seed, x_whole: int, y_whole: int, z_whole: int, x_frac: f32, y_frac: f32, z_frac: f32) -> f32 {
-    let vector = GRADIENT3[seed.get3(x_whole, y_whole, z_whole) % GRADIENT3.len()];
-    x_frac * vector[0] + y_frac * vector[1] + z_frac * vector[2]
-}
-
-fn get_perlin4_gradient(seed: &Seed, x_whole: int, y_whole: int, z_whole: int, w_whole: int, x_frac: f32, y_frac: f32, z_frac: f32, w_frac: f32) -> f32 {
-    let vector = GRADIENT4[seed.get4(x_whole, y_whole, z_whole, w_whole) % GRADIENT4.len()];
-    x_frac * vector[0] + y_frac * vector[1] + z_frac * vector[2] + w_frac * vector[3]
-}
-
-fn perlin2<T: Float>(seed: &Seed, point: &::Point2<T>, quality: ::Quality) -> f32 {
-    use Quality::{ Fast, Best };
+fn perlin2<T, F>(seed: &Seed, point: &::Point2<T>, scurve: F) -> f32 where
+    T: Float,
+    F: Fn(f32) -> f32,
+{
+    fn gradient(seed: &Seed, x_whole: int, y_whole: int, x_frac: f32, y_frac: f32) -> f32 {
+        let vector = GRADIENT2[seed.get2(x_whole, y_whole) % GRADIENT2.len()];
+        x_frac * vector[0] + y_frac * vector[1]
+    }
 
     let xfloor = point[0].floor().to_f32().unwrap();
     let yfloor = point[1].floor().to_f32().unwrap();
@@ -52,38 +43,36 @@ fn perlin2<T: Float>(seed: &Seed, point: &::Point2<T>, quality: ::Quality) -> f3
     let x1_frac = x0_frac - 1.0;
     let y1_frac = y0_frac - 1.0;
 
-    let (x_curve, y_curve) = match quality {
-        Fast => (
-            scurve3(x0_frac),
-            scurve3(y0_frac),
-        ),
-        Best => (
-            scurve5(x0_frac),
-            scurve5(y0_frac),
-        )
-    };
+    let x_curve = scurve(x0_frac);
+    let y_curve = scurve(y0_frac);
 
-    let n0 = get_perlin2_gradient(seed, x0_whole, y0_whole, x0_frac, y0_frac);
-    let n1 = get_perlin2_gradient(seed, x1_whole, y0_whole, x1_frac, y0_frac);
+    let n0 = gradient(seed, x0_whole, y0_whole, x0_frac, y0_frac);
+    let n1 = gradient(seed, x1_whole, y0_whole, x1_frac, y0_frac);
     let interpolated_x0 = lerp(x_curve, n0, n1);
 
-    let n0 = get_perlin2_gradient(seed, x0_whole, y1_whole, x0_frac, y1_frac);
-    let n1 = get_perlin2_gradient(seed, x1_whole, y1_whole, x1_frac, y1_frac);
+    let n0 = gradient(seed, x0_whole, y1_whole, x0_frac, y1_frac);
+    let n1 = gradient(seed, x1_whole, y1_whole, x1_frac, y1_frac);
     let interpolated_x1 = lerp(x_curve, n0, n1);
 
     lerp(y_curve, interpolated_x0, interpolated_x1)
 }
 
 pub fn perlin2_fast<T: Float>(seed: &Seed, point: &::Point2<T>) -> f32 {
-    perlin2(seed, point, ::Quality::Fast)
+    perlin2(seed, point, scurve3)
 }
 
 pub fn perlin2_best<T: Float>(seed: &Seed, point: &::Point2<T>) -> f32 {
-    perlin2(seed, point, ::Quality::Best)
+    perlin2(seed, point, scurve5)
 }
 
-fn perlin3<T: Float>(seed: &Seed, point: &::Point3<T>, quality: ::Quality) -> f32 {
-    use Quality::{ Fast, Best };
+fn perlin3<T, F>(seed: &Seed, point: &::Point3<T>, scurve: F) -> f32 where
+    T: Float,
+    F: Fn(f32) -> f32,
+{
+    fn gradient(seed: &Seed, x_whole: int, y_whole: int, z_whole: int, x_frac: f32, y_frac: f32, z_frac: f32) -> f32 {
+        let vector = GRADIENT3[seed.get3(x_whole, y_whole, z_whole) % GRADIENT3.len()];
+        x_frac * vector[0] + y_frac * vector[1] + z_frac * vector[2]
+    }
 
     let xfloor = point[0].floor().to_f32().unwrap();
     let yfloor = point[1].floor().to_f32().unwrap();
@@ -105,34 +94,25 @@ fn perlin3<T: Float>(seed: &Seed, point: &::Point3<T>, quality: ::Quality) -> f3
     let y1_frac = y0_frac - 1.0;
     let z1_frac = z0_frac - 1.0;
 
-    let (x_curve, y_curve, z_curve) = match quality {
-        Fast => (
-            scurve3(x0_frac),
-            scurve3(y0_frac),
-            scurve3(z0_frac),
-        ),
-        Best => (
-            scurve5(x0_frac),
-            scurve5(y0_frac),
-            scurve5(z0_frac),
-        )
-    };
+    let x_curve = scurve(x0_frac);
+    let y_curve = scurve(y0_frac);
+    let z_curve = scurve(z0_frac);
 
-    let n0 = get_perlin3_gradient(seed, x0_whole, y0_whole, z0_whole, x0_frac, y0_frac, z0_frac);
-    let n1 = get_perlin3_gradient(seed, x1_whole, y0_whole, z0_whole, x1_frac, y0_frac, z0_frac);
+    let n0 = gradient(seed, x0_whole, y0_whole, z0_whole, x0_frac, y0_frac, z0_frac);
+    let n1 = gradient(seed, x1_whole, y0_whole, z0_whole, x1_frac, y0_frac, z0_frac);
     let interpolated_x0 = lerp(x_curve, n0, n1);
 
-    let n0 = get_perlin3_gradient(seed, x0_whole, y1_whole, z0_whole, x0_frac, y1_frac, z0_frac);
-    let n1 = get_perlin3_gradient(seed, x1_whole, y1_whole, z0_whole, x1_frac, y1_frac, z0_frac);
+    let n0 = gradient(seed, x0_whole, y1_whole, z0_whole, x0_frac, y1_frac, z0_frac);
+    let n1 = gradient(seed, x1_whole, y1_whole, z0_whole, x1_frac, y1_frac, z0_frac);
     let interpolated_x1 = lerp(x_curve, n0, n1);
     let interpolated_y0 = lerp(y_curve, interpolated_x0, interpolated_x1);
 
-    let n0 = get_perlin3_gradient(seed, x0_whole, y0_whole, z1_whole, x0_frac, y0_frac, z1_frac);
-    let n1 = get_perlin3_gradient(seed, x1_whole, y0_whole, z1_whole, x1_frac, y0_frac, z1_frac);
+    let n0 = gradient(seed, x0_whole, y0_whole, z1_whole, x0_frac, y0_frac, z1_frac);
+    let n1 = gradient(seed, x1_whole, y0_whole, z1_whole, x1_frac, y0_frac, z1_frac);
     let interpolated_x0 = lerp(x_curve, n0, n1);
 
-    let n0 = get_perlin3_gradient(seed, x0_whole, y1_whole, z1_whole, x0_frac, y1_frac, z1_frac);
-    let n1 = get_perlin3_gradient(seed, x1_whole, y1_whole, z1_whole, x1_frac, y1_frac, z1_frac);
+    let n0 = gradient(seed, x0_whole, y1_whole, z1_whole, x0_frac, y1_frac, z1_frac);
+    let n1 = gradient(seed, x1_whole, y1_whole, z1_whole, x1_frac, y1_frac, z1_frac);
     let interpolated_x1 = lerp(x_curve, n0, n1);
     let interpolated_y1 = lerp(y_curve, interpolated_x0, interpolated_x1);
 
@@ -140,15 +120,21 @@ fn perlin3<T: Float>(seed: &Seed, point: &::Point3<T>, quality: ::Quality) -> f3
 }
 
 pub fn perlin3_fast<T: Float>(seed: &Seed, point: &::Point3<T>) -> f32 {
-    perlin3(seed, point, ::Quality::Fast)
+    perlin3(seed, point, scurve3)
 }
 
 pub fn perlin3_best<T: Float>(seed: &Seed, point: &::Point3<T>) -> f32 {
-    perlin3(seed, point, ::Quality::Best)
+    perlin3(seed, point, scurve5)
 }
 
-fn perlin4<T: Float>(seed: &Seed, point: &::Point4<T>, quality: ::Quality) -> f32 {
-    use Quality::{ Fast, Best };
+fn perlin4<T, F>(seed: &Seed, point: &::Point4<T>, scurve: F) -> f32 where
+    T: Float,
+    F: Fn(f32) -> f32,
+{
+    fn gradient(seed: &Seed, x_whole: int, y_whole: int, z_whole: int, w_whole: int, x_frac: f32, y_frac: f32, z_frac: f32, w_frac: f32) -> f32 {
+        let vector = GRADIENT4[seed.get4(x_whole, y_whole, z_whole, w_whole) % GRADIENT4.len()];
+        x_frac * vector[0] + y_frac * vector[1] + z_frac * vector[2] + w_frac * vector[3]
+    }
 
     let xfloor = point[0].floor().to_f32().unwrap();
     let yfloor = point[1].floor().to_f32().unwrap();
@@ -175,55 +161,45 @@ fn perlin4<T: Float>(seed: &Seed, point: &::Point4<T>, quality: ::Quality) -> f3
     let z1_frac = z0_frac - 1.0;
     let w1_frac = w0_frac - 1.0;
 
-    let (x_curve, y_curve, z_curve, w_curve) = match quality {
-        Fast => (
-            scurve3(x0_frac),
-            scurve3(y0_frac),
-            scurve3(z0_frac),
-            scurve3(w0_frac),
-        ),
-        Best => (
-            scurve5(x0_frac),
-            scurve5(y0_frac),
-            scurve5(z0_frac),
-            scurve5(w0_frac),
-        )
-    };
+    let x_curve = scurve(x0_frac);
+    let y_curve = scurve(y0_frac);
+    let z_curve = scurve(z0_frac);
+    let w_curve = scurve(w0_frac);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y0_whole, z0_whole, w0_whole, x0_frac, y0_frac, z0_frac, w0_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y0_whole, z0_whole, w0_whole, x1_frac, y0_frac, z0_frac, w0_frac);
+    let n0 = gradient(seed, x0_whole, y0_whole, z0_whole, w0_whole, x0_frac, y0_frac, z0_frac, w0_frac);
+    let n1 = gradient(seed, x1_whole, y0_whole, z0_whole, w0_whole, x1_frac, y0_frac, z0_frac, w0_frac);
     let interpolated_x0 = lerp(x_curve, n0, n1);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y1_whole, z0_whole, w0_whole, x0_frac, y1_frac, z0_frac, w0_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y1_whole, z0_whole, w0_whole, x1_frac, y1_frac, z0_frac, w0_frac);
+    let n0 = gradient(seed, x0_whole, y1_whole, z0_whole, w0_whole, x0_frac, y1_frac, z0_frac, w0_frac);
+    let n1 = gradient(seed, x1_whole, y1_whole, z0_whole, w0_whole, x1_frac, y1_frac, z0_frac, w0_frac);
     let interpolated_x1 = lerp(x_curve, n0, n1);
     let interpolated_y0 = lerp(y_curve, interpolated_x0, interpolated_x1);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y0_whole, z1_whole, w0_whole, x0_frac, y0_frac, z1_frac, w0_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y0_whole, z1_whole, w0_whole, x1_frac, y0_frac, z1_frac, w0_frac);
+    let n0 = gradient(seed, x0_whole, y0_whole, z1_whole, w0_whole, x0_frac, y0_frac, z1_frac, w0_frac);
+    let n1 = gradient(seed, x1_whole, y0_whole, z1_whole, w0_whole, x1_frac, y0_frac, z1_frac, w0_frac);
     let interpolated_x0 = lerp(x_curve, n0, n1);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y1_whole, z1_whole, w0_whole, x0_frac, y1_frac, z1_frac, w0_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y1_whole, z1_whole, w0_whole, x1_frac, y1_frac, z1_frac, w0_frac);
+    let n0 = gradient(seed, x0_whole, y1_whole, z1_whole, w0_whole, x0_frac, y1_frac, z1_frac, w0_frac);
+    let n1 = gradient(seed, x1_whole, y1_whole, z1_whole, w0_whole, x1_frac, y1_frac, z1_frac, w0_frac);
     let interpolated_x1 = lerp(x_curve, n0, n1);
     let interpolated_y1 = lerp(y_curve, interpolated_x0, interpolated_x1);
     let interpolated_z0 = lerp(z_curve, interpolated_y0, interpolated_y1);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y0_whole, z0_whole, w1_whole, x0_frac, y0_frac, z0_frac, w1_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y0_whole, z0_whole, w1_whole, x1_frac, y0_frac, z0_frac, w1_frac);
+    let n0 = gradient(seed, x0_whole, y0_whole, z0_whole, w1_whole, x0_frac, y0_frac, z0_frac, w1_frac);
+    let n1 = gradient(seed, x1_whole, y0_whole, z0_whole, w1_whole, x1_frac, y0_frac, z0_frac, w1_frac);
     let interpolated_x0 = lerp(x_curve, n0, n1);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y1_whole, z0_whole, w1_whole, x0_frac, y1_frac, z0_frac, w1_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y1_whole, z0_whole, w1_whole, x1_frac, y1_frac, z0_frac, w1_frac);
+    let n0 = gradient(seed, x0_whole, y1_whole, z0_whole, w1_whole, x0_frac, y1_frac, z0_frac, w1_frac);
+    let n1 = gradient(seed, x1_whole, y1_whole, z0_whole, w1_whole, x1_frac, y1_frac, z0_frac, w1_frac);
     let interpolated_x1 = lerp(x_curve, n0, n1);
     let interpolated_y0 = lerp(y_curve, interpolated_x0, interpolated_x1);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y0_whole, z1_whole, w1_whole, x0_frac, y0_frac, z1_frac, w1_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y0_whole, z1_whole, w1_whole, x1_frac, y0_frac, z1_frac, w1_frac);
+    let n0 = gradient(seed, x0_whole, y0_whole, z1_whole, w1_whole, x0_frac, y0_frac, z1_frac, w1_frac);
+    let n1 = gradient(seed, x1_whole, y0_whole, z1_whole, w1_whole, x1_frac, y0_frac, z1_frac, w1_frac);
     let interpolated_x0 = lerp(x_curve, n0, n1);
 
-    let n0 = get_perlin4_gradient(seed, x0_whole, y1_whole, z1_whole, w1_whole, x0_frac, y1_frac, z1_frac, w1_frac);
-    let n1 = get_perlin4_gradient(seed, x1_whole, y1_whole, z1_whole, w1_whole, x1_frac, y1_frac, z1_frac, w1_frac);
+    let n0 = gradient(seed, x0_whole, y1_whole, z1_whole, w1_whole, x0_frac, y1_frac, z1_frac, w1_frac);
+    let n1 = gradient(seed, x1_whole, y1_whole, z1_whole, w1_whole, x1_frac, y1_frac, z1_frac, w1_frac);
     let interpolated_x1 = lerp(x_curve, n0, n1);
     let interpolated_y1 = lerp(y_curve, interpolated_x0, interpolated_x1);
     let interpolated_z1 = lerp(z_curve, interpolated_y0, interpolated_y1);
@@ -232,9 +208,9 @@ fn perlin4<T: Float>(seed: &Seed, point: &::Point4<T>, quality: ::Quality) -> f3
 }
 
 pub fn perlin4_fast<T: Float>(seed: &Seed, point: &::Point4<T>) -> f32 {
-    perlin4(seed, point, ::Quality::Fast)
+    perlin4(seed, point, scurve3)
 }
 
 pub fn perlin4_best<T: Float>(seed: &Seed, point: &::Point4<T>) -> f32 {
-    perlin4(seed, point, ::Quality::Best)
+    perlin4(seed, point, scurve5)
 }
