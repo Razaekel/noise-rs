2d2c18bae5d51aa0af5d482bfdcf3f1e2ecc553b
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-12-27 17:43:12 +1000

    Impl std::rand::Rand for Seed

--- src/seed.rs
+++ src/seed.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::rand;
+use std::rand::{Rand, Rng, SeedableRng, XorShiftRng};
 
 use util::signed_modulus;
 
@@ -21,17 +21,17 @@ const TABLE_SIZE: uint = 256;
 
 #[allow(missing_copy_implementations)]
 pub struct Seed {
-    values: [uint, ..TABLE_SIZE*2],
+    values: [uint, ..TABLE_SIZE * 2],
 }
 
-impl Seed {
-    pub fn from_rng<R: rand::Rng>(rng: &mut R) -> Seed {
+impl Rand for Seed {
+    fn rand<R: Rng>(rng: &mut R) -> Seed {
         let mut seq: Vec<uint> = range(0u, TABLE_SIZE).collect();
         for i in range(0, TABLE_SIZE) {
-            let mut swap_i: uint = rand::Rand::rand(rng);
+            let mut swap_i: uint = rng.gen();
             swap_i = swap_i % TABLE_SIZE;
-            let swap = seq[swap_i as uint];
-            seq[swap_i as uint] = seq[i];
+            let swap = seq[swap_i];
+            seq[swap_i] = seq[i];
             seq[i] = swap;
         }
 
@@ -39,17 +39,19 @@ impl Seed {
         // clean way to do this in one pass. Hopefully won't matter, as Seed creation will usually be a
         // one-time event.
         let mut new_seed = Seed {
-            values: [0, ..TABLE_SIZE*2],
+            values: [0, ..TABLE_SIZE * 2],
         };
-        for i in range(0, TABLE_SIZE*2) {
+        for i in range(0, TABLE_SIZE * 2) {
             new_seed.values[i] = seq[i % TABLE_SIZE];
         }
         new_seed
     }
+}
 
+impl Seed {
     pub fn new(seed: u32) -> Seed {
-        let mut rng: rand::XorShiftRng = rand::SeedableRng::from_seed([1, seed, seed, seed]);
-        Seed::from_rng(&mut rng)
+        let mut rng: XorShiftRng = SeedableRng::from_seed([1, seed, seed, seed]);
+        Rand::rand(&mut rng)
     }
 
     pub fn get1(&self, x: int) -> uint {
@@ -66,5 +68,16 @@ impl Seed {
 
     pub fn get4(&self, x: int, y: int, z: int, w: int) -> uint {
         self.values[(signed_modulus(w, TABLE_SIZE as int)) + self.get3(x, y, z)]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::rand::random;
+    use perlin::perlin3_best;
+
+    #[test]
+    fn test_random_seed() {
+        let _ = perlin3_best::<f32>(&random(), &[1.0, 2.0, 3.0]);
     }
 }
