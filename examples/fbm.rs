60131a5e29d095b309c5f384c0295f5a376761b8
Nick Whitney <ubersentinel@gmail.com>
2016-08-07 10:35:08 -0400

    Add fBm noise

--- examples/fbm.rs
+++ examples/fbm.rs
@@ -1,4 +1,4 @@
-// Copyright 2015 The Noise-rs Developers.
+// Copyright 2016 The Noise-rs Developers.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use self::checkerboard::*;
-pub use self::constant::*;
-pub use self::cylinders::*;
-pub use self::fractals::*;
-pub use self::perlin::*;
+//! An example of using perlin noise
 
-mod constant;
-mod checkerboard;
-mod cylinders;
-mod fractals;
-mod perlin;
+extern crate noise;
+
+use noise::modules::Fbm;
+
+mod debug;
+
+fn main() {
+    debug::render_png2("fbm.png", Fbm::new(), 1024, 1024, 400);
+}
--- src/modules/generators/fractals/mod.rs
+++ src/modules/generators/fractals/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2015 The Noise-rs Developers.
+// Copyright 2016 The Noise-rs Developers.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,14 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub use self::checkerboard::*;
-pub use self::constant::*;
-pub use self::cylinders::*;
-pub use self::fractals::*;
-pub use self::perlin::*;
+pub use self::fbm::*;
 
-mod constant;
-mod checkerboard;
-mod cylinders;
-mod fractals;
-mod perlin;
+mod fbm;
+mod util;
