051f8f7180bf0daf64d71bf22dc8c2337fa0b70e
adudney <adudney@users.noreply.github.com>
2017-06-02 19:12:44 -0700

    Implement Super Simplex noise (#168)
    * Added Super Simplex 2D
    
    * Fixed attenuation factor, refactored names of constants and variables to hopefully make more sense
    
    * Moved from integer for loop to slice based for loop
    
    * Replaced casts from float to int with floating point comparisons, almost 2x faster overall
    
    * Added calculated maximum value for 2D super simplex noise
    
    * Added 3D super simplex noise, unscaled
    
    * Added scaling factor for Super Simplex 3D
    
    * Reverted debug changes to other modules
    
    * Updated to new NoiseModule generic
    
    * Updated example to fit new debug module
    
    * Removed Mathematica file
    
    * Fixed seed desync

--- src/modules/generators/mod.rs
+++ src/modules/generators/mod.rs
@@ -12,6 +12,7 @@ pub use self::cylinders::*;
 pub use self::fractals::*;
 pub use self::open_simplex::*;
 pub use self::perlin::*;
+pub use self::super_simplex::*;
 pub use self::value::*;
 pub use self::worley::*;
 
@@ -21,5 +22,6 @@ mod cylinders;
 mod fractals;
 mod open_simplex;
 mod perlin;
+mod super_simplex;
 mod value;
 mod worley;
