068517e501b5f49946b62dfc4f5d1181c56ed111
Andrew Dudney <andrew.dudney@gmail.com>
2017-06-04 10:23:35 -0700

    Updated Perlin and Worley examples to output 2D, 3D, and 4D versions of their images

--- examples/perlin.rs
+++ examples/perlin.rs
@@ -21,6 +21,10 @@ use noise::{Perlin, Seedable};
 mod debug;
 
 fn main() {
-    debug::render_noise_module3("perlin1.png", &Perlin::new(), 1024, 1024, 50);
-    debug::render_noise_module3("perlin2.png", &Perlin::new().set_seed(1), 1024, 1024, 50);
+    debug::render_noise_module2("perlin_2d.png", &Perlin::new(), 1024, 1024, 50);
+    debug::render_noise_module2("perlin_2d_seeded.png", &Perlin::new().set_seed(1), 1024, 1024, 50);
+    debug::render_noise_module3("perlin_3d.png", &Perlin::new(), 1024, 1024, 50);
+    debug::render_noise_module3("perlin_3d_seeded.png", &Perlin::new().set_seed(1), 1024, 1024, 50);
+    debug::render_noise_module4("perlin_4d.png", &Perlin::new(), 1024, 1024, 50);
+    debug::render_noise_module4("perlin_4d_seeded.png", &Perlin::new().set_seed(1), 1024, 1024, 50);
 }
--- examples/worley.rs
+++ examples/worley.rs
@@ -21,51 +21,39 @@ use noise::{RangeFunction, Worley};
 mod debug;
 
 fn main() {
-    debug::render_noise_module3("worley-linear.png", &Worley::new(), 1024, 1024, 50);
-    debug::render_noise_module3("worley-linear-range.png",
-                               &Worley::new().enable_range(true),
-                               1024,
-                               1024,
-                               50);
-    debug::render_noise_module3("worley-linear-squared.png",
-                               &Worley::new().set_range_function(RangeFunction::EuclideanSquared),
-                               1024,
-                               1024,
-                               50);
-    debug::render_noise_module3("worley-manhattan.png",
-                               &Worley::new().set_range_function(RangeFunction::Manhattan),
-                               1024,
-                               1024,
-                               50);
-    debug::render_noise_module3("worley-manhattan-range.png",
-                               &Worley::new()
-                                   .enable_range(true)
-                                   .set_range_function(RangeFunction::Manhattan),
-                               1024,
-                               1024,
-                               50);
-    debug::render_noise_module3("worley-chebyshev.png",
-                               &Worley::new().set_range_function(RangeFunction::Chebyshev),
-                               1024,
-                               1024,
-                               50);
-    debug::render_noise_module3("worley-chebyshev-range.png",
-                               &Worley::new()
-                                   .enable_range(true)
-                                   .set_range_function(RangeFunction::Chebyshev),
-                               1024,
-                               1024,
-                               50);
-    debug::render_noise_module3("worley-quadratic.png",
-                               &Worley::new().set_range_function(RangeFunction::Quadratic),
-                               1024,
-                               1024,
-                               50);
-    debug::render_noise_module3("worley-quadratic-range.png",
-                               &Worley::new()
-                                   .enable_range(true)
-                                   .set_range_function(RangeFunction::Quadratic),
-                               1024,
-                               1024,
-                               50);
+    debug::render_noise_module2("worley-linear_2d.png", &Worley::new(), 1024, 1024, 50);
+    debug::render_noise_module3("worley-linear_3d.png", &Worley::new(), 1024, 1024, 50);
+    debug::render_noise_module4("worley-linear_4d.png", &Worley::new(), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-linear-range_2d.png", &Worley::new().enable_range(true), 1024, 1024, 50);
+    debug::render_noise_module3("worley-linear-range_3d.png", &Worley::new().enable_range(true), 1024, 1024, 50);
+    debug::render_noise_module4("worley-linear-range_4d.png", &Worley::new().enable_range(true), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-linear-squared_2d.png", &Worley::new().set_range_function(RangeFunction::EuclideanSquared), 1024, 1024, 50);
+    debug::render_noise_module3("worley-linear-squared_3d.png", &Worley::new().set_range_function(RangeFunction::EuclideanSquared), 1024, 1024, 50);
+    debug::render_noise_module4("worley-linear-squared_4d.png", &Worley::new().set_range_function(RangeFunction::EuclideanSquared), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-manhattan_2d.png", &Worley::new().set_range_function(RangeFunction::Manhattan), 1024, 1024, 50);
+    debug::render_noise_module3("worley-manhattan_3d.png", &Worley::new().set_range_function(RangeFunction::Manhattan), 1024, 1024, 50);
+    debug::render_noise_module4("worley-manhattan_4d.png", &Worley::new().set_range_function(RangeFunction::Manhattan), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-manhattan-range_2d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Manhattan), 1024, 1024, 50);
+    debug::render_noise_module3("worley-manhattan-range_3d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Manhattan), 1024, 1024, 50);
+    debug::render_noise_module4("worley-manhattan-range_4d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Manhattan), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-chebyshev_2d.png", &Worley::new().set_range_function(RangeFunction::Chebyshev), 1024, 1024, 50);
+    debug::render_noise_module3("worley-chebyshev_3d.png", &Worley::new().set_range_function(RangeFunction::Chebyshev), 1024, 1024, 50);
+    debug::render_noise_module4("worley-chebyshev_4d.png", &Worley::new().set_range_function(RangeFunction::Chebyshev), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-chebyshev-range_2d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Chebyshev), 1024, 1024, 50);
+    debug::render_noise_module3("worley-chebyshev-range_3d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Chebyshev), 1024, 1024, 50);
+    debug::render_noise_module4("worley-chebyshev-range_4d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Chebyshev), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-quadratic_2d.png", &Worley::new().set_range_function(RangeFunction::Quadratic), 1024, 1024, 50);
+    debug::render_noise_module3("worley-quadratic_3d.png", &Worley::new().set_range_function(RangeFunction::Quadratic), 1024, 1024, 50);
+    debug::render_noise_module4("worley-quadratic_4d.png", &Worley::new().set_range_function(RangeFunction::Quadratic), 1024, 1024, 50);
+
+    debug::render_noise_module2("worley-quadratic-range_2d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Quadratic), 1024, 1024, 50);
+    debug::render_noise_module3("worley-quadratic-range_3d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Quadratic), 1024, 1024, 50);
+    debug::render_noise_module4("worley-quadratic-range_4d.png", &Worley::new().enable_range(true).set_range_function(RangeFunction::Quadratic), 1024, 1024, 50);
 }
