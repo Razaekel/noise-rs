22449a2aea5881dc7f17d95ad59bb7f45320dc20
Nick Whitney <ubersentinel@gmail.com>
2018-03-01 21:14:56 -0500

    Strip License Headers
    Remove all license headers from the files. Licenses will be on a repo-basis instead.

--- src/noise_fns/generators/open_simplex.rs
+++ src/noise_fns/generators/open_simplex.rs
@@ -1,11 +1,3 @@
-// Copyright (c) 2017 The Noise-rs Developers.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
-// or http://opensource.org/licenses/MIT>, at your option. All files in the
-// project carrying such notice may not be copied, modified, or distributed
-// except according to those terms.
-
 //! Note that this is NOT Ken Perlin's simplex noise, as that is patent encumbered.
 //! Instead, these functions use the `OpenSimplex` algorithm, as detailed here:
 //! http://uniblock.tumblr.com/post/97868843242/noise
