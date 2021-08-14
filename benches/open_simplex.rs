98352080b988455ed16300b1ef5eab8549cef8e0
Aubrey Powell <herufeanor@gmail.com>
2021-08-14 16:01:14 -0700

    Fix compilation errors in the benchmarks

--- benches/open_simplex.rs
+++ benches/open_simplex.rs
@@ -20,28 +20,28 @@ criterion_group!(
 criterion_main!(open_simplex, open_simplex_64x64);
 
 fn bench_open_simplex2(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new();
+    let open_simplex = OpenSimplex::new(42);
     c.bench_function("open simplex 2d", |b| {
         b.iter(|| open_simplex.get(black_box([42.0_f64, 37.0])))
     });
 }
 
 fn bench_open_simplex3(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new();
+    let open_simplex = OpenSimplex::new(42);
     c.bench_function("open simplex 3d", |b| {
         b.iter(|| open_simplex.get(black_box([42.0_f64, 37.0, 26.0])))
     });
 }
 
 fn bench_open_simplex4(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new();
+    let open_simplex = OpenSimplex::new(42);
     c.bench_function("open simplex 4d", |b| {
         b.iter(|| open_simplex.get(black_box([42.0_f64, 37.0, 26.0, 128.0])))
     });
 }
 
 fn bench_open_simplex2_64x64(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new();
+    let open_simplex = OpenSimplex::new(42);
     c.bench_function("open simplex 2d (64x64)", |b| {
         b.iter(|| {
             for y in 0i8..64 {
@@ -54,7 +54,7 @@ fn bench_open_simplex2_64x64(c: &mut Criterion) {
 }
 
 fn bench_open_simplex3_64x64(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new();
+    let open_simplex = OpenSimplex::new(42);
     c.bench_function("open simplex 3d (64x64)", |b| {
         b.iter(|| {
             for y in 0i8..64 {
@@ -67,7 +67,7 @@ fn bench_open_simplex3_64x64(c: &mut Criterion) {
 }
 
 fn bench_open_simplex4_64x64(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new();
+    let open_simplex = OpenSimplex::new(42);
     c.bench_function("open simplex 4d (64x64)", |b| {
         b.iter(|| {
             for y in 0i8..64 {
