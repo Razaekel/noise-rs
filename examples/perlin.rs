577408fce9909dc68e310ace09f123df85a54c2e
Aubrey Powell <herufeanor@gmail.com>
2021-08-15 02:41:53 -0700

    Perlin example outputs 2D, 3D and 4D images
    This required expanding the PlaneMapBuilder a little bit to support
    varying dimensions of noise functions. But now the example can be used
    to validate that all dimensions of perlin noise continue to output
    correct results.

--- examples/perlin.rs
+++ examples/perlin.rs
@@ -2,23 +2,46 @@
 
 extern crate noise;
 
-use noise::{utils::*, core::perlin::perlin_3d, permutationtable::PermutationTable};
+use noise::{utils::*, core::perlin::{perlin_2d, perlin_3d, perlin_4d}, permutationtable::PermutationTable};
 
 fn main() {
     let hasher = PermutationTable::new(0);
-    
+    PlaneMapBuilder::new_fn(perlin_2d, &hasher)
+        .set_size(1024, 1024)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build()
+        .write_to_file("perlin_2d_seed=0.png");
     PlaneMapBuilder::new_fn(perlin_3d, &hasher)
         .set_size(1024, 1024)
         .set_x_bounds(-5.0, 5.0)
         .set_y_bounds(-5.0, 5.0)
         .build()
-        .write_to_file("perlin_seed=0.png");
-    
+        .write_to_file("perlin_3d_seed=0.png");
+    PlaneMapBuilder::new_fn(perlin_4d, &hasher)
+        .set_size(1024, 1024)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build()
+        .write_to_file("perlin_4d_seed=0.png");
+
     let hasher = PermutationTable::new(1);
+    PlaneMapBuilder::new_fn(perlin_2d, &hasher)
+        .set_size(1024, 1024)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build()
+        .write_to_file("perlin_2d_seed=1.png");
     PlaneMapBuilder::new_fn(perlin_3d, &hasher)
         .set_size(1024, 1024)
         .set_x_bounds(-5.0, 5.0)
         .set_y_bounds(-5.0, 5.0)
         .build()
-        .write_to_file("perlin_seed=1.png");
+        .write_to_file("perlin_3d_seed=1.png");
+    PlaneMapBuilder::new_fn(perlin_4d, &hasher)
+        .set_size(1024, 1024)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build()
+        .write_to_file("perlin_4d_seed=1.png");
 }
