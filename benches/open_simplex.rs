b2867f8fc5223f120450845709a3f67db5a99ebe
Aubrey Powell <herufeanor@gmail.com>
2021-08-16 12:22:35 -0700

    OpenSimplex benches now call core functions

--- benches/open_simplex.rs
+++ benches/open_simplex.rs
@@ -3,7 +3,10 @@ extern crate criterion;
 extern crate noise;
 
 use criterion::{black_box, Criterion};
-use noise::{NoiseFn, OpenSimplex};
+use noise::{
+    core::open_simplex::{open_simplex_2d, open_simplex_3d, open_simplex_4d},
+    permutationtable::PermutationTable,
+};
 
 criterion_group!(
     open_simplex,
@@ -20,33 +23,33 @@ criterion_group!(
 criterion_main!(open_simplex, open_simplex_64x64);
 
 fn bench_open_simplex2(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new(42);
+    let hasher = PermutationTable::new(0);
     c.bench_function("open simplex 2d", |b| {
-        b.iter(|| open_simplex.get(black_box([42.0_f64, 37.0])))
+        b.iter(|| black_box(open_simplex_2d([42.0_f64, 37.0], &hasher)))
     });
 }
 
 fn bench_open_simplex3(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new(42);
+    let hasher = PermutationTable::new(0);
     c.bench_function("open simplex 3d", |b| {
-        b.iter(|| open_simplex.get(black_box([42.0_f64, 37.0, 26.0])))
+        b.iter(|| black_box(open_simplex_3d([42.0_f64, 37.0, 26.0], &hasher)))
     });
 }
 
 fn bench_open_simplex4(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new(42);
+    let hasher = PermutationTable::new(0);
     c.bench_function("open simplex 4d", |b| {
-        b.iter(|| open_simplex.get(black_box([42.0_f64, 37.0, 26.0, 128.0])))
+        b.iter(|| black_box(open_simplex_4d([42.0_f64, 37.0, 26.0, 128.0], &hasher)))
     });
 }
 
 fn bench_open_simplex2_64x64(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new(42);
+    let hasher = PermutationTable::new(0);
     c.bench_function("open simplex 2d (64x64)", |b| {
         b.iter(|| {
             for y in 0i8..64 {
                 for x in 0i8..64 {
-                    black_box(open_simplex.get([x as f64, y as f64]));
+                    black_box(open_simplex_2d([x as f64, y as f64], &hasher));
                 }
             }
         })
@@ -54,12 +57,12 @@ fn bench_open_simplex2_64x64(c: &mut Criterion) {
 }
 
 fn bench_open_simplex3_64x64(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new(42);
+    let hasher = PermutationTable::new(0);
     c.bench_function("open simplex 3d (64x64)", |b| {
         b.iter(|| {
             for y in 0i8..64 {
                 for x in 0i8..64 {
-                    black_box(open_simplex.get([x as f64, y as f64, x as f64]));
+                    black_box(open_simplex_3d([x as f64, y as f64, x as f64], &hasher));
                 }
             }
         })
@@ -67,12 +70,12 @@ fn bench_open_simplex3_64x64(c: &mut Criterion) {
 }
 
 fn bench_open_simplex4_64x64(c: &mut Criterion) {
-    let open_simplex = OpenSimplex::new(42);
+    let hasher = PermutationTable::new(0);
     c.bench_function("open simplex 4d (64x64)", |b| {
         b.iter(|| {
             for y in 0i8..64 {
                 for x in 0i8..64 {
-                    black_box(open_simplex.get([x as f64, y as f64, x as f64, y as f64]));
+                    black_box(open_simplex_4d([x as f64, y as f64, x as f64, y as f64], &hasher));
                 }
             }
         })
